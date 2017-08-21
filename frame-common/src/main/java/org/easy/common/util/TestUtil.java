cb499928e2623e95b710512fe6ae5110e65fa293
xiaojianyu <xiaojianyu@9fbank.cc>
2017-08-21 09:53:35 +0800

    完善分库分表

--- frame-common/src/main/java/org/easy/common/util/TestUtil.java
+++ frame-common/src/main/java/org/easy/common/util/TestUtil.java
@@ -4,6 +4,9 @@ import com.xiaoleilu.hutool.date.DateUtil;
 import com.xiaoleilu.hutool.http.HttpRequest;
 import com.xiaoleilu.hutool.http.HttpUtil;
 import com.xiaoleilu.hutool.json.JSONUtil;
+import com.xiaoleilu.hutool.util.XmlUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 /**
  * Created by xiaojianyu on 2017/8/16.
